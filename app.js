01521932639ff415c6a289ea2e63c9e514a0bba8
Anh_Le <lehoangmaianh126@gmail.com>
2021-10-04 18:55:24 -0400

    Modify size of style.css, add location, info, evolution, moves func

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
-const baseURL = "https://pokeapi.co/api/v2"
+const baseURL = "https://pokeapi.co/api/v2";
+
+let pokemonArray = new Array();
+
+
+document.addEventListener('keypress', e => {
+    if (e.key === "Enter") {
+        let pokemonID = document.getElementById("pokemonID").value;
+        printInfo(pokemonID);
+    }
+})
+
+document.addEventListener('click', c => {
+    buildPokemonArray();
+    if (c.target === document.getElementById("MOVES")) {
+        let pokemonID = document.getElementById("pokemonID").value;
+        printMoves(pokemonID);
+    } else if (c.target === document.getElementById("LOCATION")) {
+        let pokemonID = document.getElementById("pokemonID").value;
+        printLocation(pokemonID);
+    } else if (c.target === document.getElementById("INFO")) {
+        let pokemonID = document.getElementById("pokemonID").value;
+        printInfo(pokemonID);
+    } else if (c.target === document.getElementById("EVOLUTION")) {
+        let pokemonID = document.getElementById("pokemonID").value;
+        printEvolution(pokemonID);
+    } 
+})
 
 
 async function printInfo(pokemonID) {
     const response = await fetch(baseURL + "/pokemon/" + pokemonID);
     const infoData = await response.json();
-    console.log("INFO");
-    console.log("height: " + infoData.height + " m");
-    console.log("weight: " + infoData.weight + " lbs");
+
+    let height = "height: " + infoData.height + " m<br/>";
+    let weight = "weight: " + infoData.weight + " lbs<br/>";
+    let stats = "";
+
     for (let i = 0; i < infoData.stats.length; i++) {
-        console.log(infoData.stats[i].stat.name + ": " + infoData.stats[i].base_stat);
+        stats += infoData.stats[i].stat.name + ": " + infoData.stats[i].base_stat + "<br/>";
     }
+
+    document.getElementById("projectTitle").innerHTML = "INFO<br/>" + height + weight + stats;
+
 }
 
 async function printMoves(pokemonID) {
     const response = await fetch(baseURL + "/pokemon/" + pokemonID);
     const infoData = await response.json();
-    console.log("MOVES");
+    let moves = "";
+
     for (let i = 0; i < infoData.moves.length; i++) {
-        console.log(infoData.moves[i].move.name);
+        moves += infoData.moves[i].move.name + "<br/>";
     }
+
+    document.getElementById("projectTitle").innerHTML = "MOVES<br/>" + moves;
 }
 
 async function printLocation(pokemonID) {
     const locationData = await fetch (baseURL + "/pokemon/" + pokemonID + "/encounters");
-    console.log("LOCATIONS");
+
+    let locationList = "";
     const locations = await locationData.json();
+
     for (let i = 0; i < locations.length; i++) {
-        console.log(locations[i].location_area.name);
+        locationList += locations[i].location_area.name + "<br/>";
     }
+
+    if (locationList === "") {
+        locationList = "CAN NOT BE CAUGHT IN THE WILD<br/>";
+    }
+
+    document.getElementById("projectTitle").innerHTML = "LOCATIONS<br/>" + locationList;
 }
 
 async function printEvolution(pokemonID) {
@@ -38,30 +81,26 @@ async function printEvolution(pokemonID) {
     const getEvolutionData = await fetch(evolutionURL);
     const evolutionData = await getEvolutionData.json();
 
-    let resultChain = new Array();
-    let counter = 0;
-
-    //Add the pokemon with the pokemonID as the first element in the chain
-    resultChain[counter] = evolutionData.chain.species.name;
-    counter++;
-
+    let resultEvolution = "";
+    resultEvolution += evolutionData.chain.species.name + "<br/>";
     let evolutionChain = evolutionData.chain.evolves_to;
-    
 
     while (evolutionChain.length > 0) {
-        resultChain[counter] = evolutionChain[0].species.name;
-        counter++;
+        resultEvolution += evolutionChain[0].species.name + "<br/>";
         evolutionChain = evolutionChain[0].evolves_to;
     }
-
-    console.log(resultChain);
+    
+    document.getElementById("projectTitle").innerHTML = "EVOLUTION<br/>" + resultEvolution;
 
 }
 
-async function main() {
-    printInfo("bulbasaur");
-    printMoves("bulbasaur");
-    printLocation("bulbasaur");
-    printEvolution("bulbasaur");
+async function buildPokemonArray() {
+
+    const initialResponse = await fetch(baseURL + "/pokemon");
+    const parsedResponse = await initialResponse.json();
+    let count = 0;
+    for (let i = 0; i < parsedResponse.results.length; i++) {
+        pokemonArray[i] = parsedResponse.results[i].name;
+    }
 }
-main()
+
