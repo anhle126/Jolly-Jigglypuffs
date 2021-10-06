cb410b71ce7f49875c246d1e05917fa670e5541e
Anh_Le <lehoangmaianh126@gmail.com>
2021-10-06 13:29:20 -0400

    Fixed functionalities. Search stil have some delay

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const baseURL = 'https://pokeapi.co/api/v2';
 
 let pokemonArray = new Array();
 
+async function main() {
+  const initialResponse = await fetch(baseURL + '/pokemon?offset=0&limit=151');
+  const parsedResponse = await initialResponse.json();
+  let count = 0;
+  for (let i = 0; i < parsedResponse.results.length; i++) {
+    pokemonArray[i] = parsedResponse.results[i].name;
+  }
+  console.log(pokemonArray);
+}
+
+main();
+
 document.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     document.getElementById('projectTitle').style.fontSize = 'x-large';
@@ -12,7 +24,6 @@ document.addEventListener('keypress', (e) => {
 });
 
 document.addEventListener('click', (c) => {
-  buildPokemonArray();
   if (c.target === document.getElementById('MOVES')) {
     let pokemonID = document.getElementById('search').value;
     printMoves(pokemonID);
@@ -35,6 +46,7 @@ document.addEventListener('click', (c) => {
         break;
       }
     }
+
     let prevPokemon = pokemonArray[modulo(index-1, pokemonArray.length)];
     document.getElementById('search').value = prevPokemon;
     printInfo(prevPokemon);
@@ -59,31 +71,17 @@ document.addEventListener('click', (c) => {
 async function printInfo(pokemonID) {
   const response = await fetch(baseURL + '/pokemon/' + pokemonID);
   const infoData = await response.json();
-  //console.log(infoData);
 
   let height = 'height: ' + infoData.height + ' m<br/>';
   let weight = 'weight: ' + infoData.weight + ' lbs<br/>';
   let stats = '';
 
   for (let i = 0; i < infoData.stats.length; i++) {
-    stats +=
-      infoData.stats[i].stat.name +
-      ': ' +
-      infoData.stats[i].base_stat +
-      '<br/>';
+    stats += infoData.stats[i].stat.name + ': ' + infoData.stats[i].base_stat + '<br/>';
   }
 
-  document.getElementById('projectTitle').innerHTML =
-    'INFO<br/><br/>' + height + weight + stats;
-  //printPhoto(pokemonID);
-
-  document.getElementById('search').value = infoData.name.charAt(0).toUpperCase() + infoData.name.substr(1);
-  document.getElementById('type1').innerHTML = infoData.types[0].type.name.charAt(0).toUpperCase() + infoData.types[0].type.name.substr(1);
-  if (infoData.types.length === 2) {
-    document.getElementById('type2').innerHTML = infoData.types[1].type.name.charAt().toUpperCase() + infoData.types[1].type.name.substr(1);
-  } else {
-    document.getElementById('type2').innerHTML = 'None';
-  }
+  document.getElementById('projectTitle').innerHTML = 'INFO<br/><br/>' + height + weight + stats;
+  printPhoto(pokemonID);
 }
 
 
@@ -93,11 +91,11 @@ async function printMoves(pokemonID) {
   let moves = '';
 
   for (let i = 0; i < infoData.moves.length; i++) {
-    moves += infoData.moves[i].move.name.replace(/-/g, ' ') + '<br/>';
+    moves += infoData.moves[i].move.name + '<br/>';
   }
 
   document.getElementById('projectTitle').innerHTML = 'MOVES<br/><br/>' + moves;
-  //printPhoto(pokemonID);
+  printPhoto(pokemonID);
 }
 
 async function printLocation(pokemonID) {
@@ -107,19 +105,17 @@ async function printLocation(pokemonID) {
 
   let locationList = '';
   const locations = await locationData.json();
-  //console.log(locations)
 
   for (let i = 0; i < locations.length; i++) {
-    locationList += locations[i].location_area.name.replace(/-/g, ' ') + '<br/>';
+    locationList += locations[i].location_area.name + '<br/>';
   }
 
   if (locationList === '') {
     locationList = 'CANNOT BE CAUGHT IN THE WILD<br/>';
   }
 
-  document.getElementById('projectTitle').innerHTML =
-    'LOCATIONS<br/><br/>' + locationList;
-  //printPhoto(pokemonID);
+  document.getElementById('projectTitle').innerHTML = 'LOCATIONS<br/><br/>' + locationList;
+  printPhoto(pokemonID);
 }
 
 async function printEvolution(pokemonID) {
@@ -128,7 +124,7 @@ async function printEvolution(pokemonID) {
   const evolutionURL = evolution.evolution_chain.url;
   const getEvolutionData = await fetch(evolutionURL);
   const evolutionData = await getEvolutionData.json();
-  //console.log(evolutionData);
+
   let resultEvolution = '';
   resultEvolution += evolutionData.chain.species.name + '<br/>';
   let evolutionChain = evolutionData.chain.evolves_to;
@@ -139,19 +135,9 @@ async function printEvolution(pokemonID) {
   }
 
   document.getElementById('projectTitle').innerHTML = 'EVOLUTION<br/><br/>' + resultEvolution;
-  //printPhoto(pokemonID);
+  printPhoto(pokemonID);
 }
 
-async function buildPokemonArray() {
-
-  const initialResponse = await fetch(baseURL + '/pokemon?offset=0&limit=151');
-  const parsedResponse = await initialResponse.json();
-  let count = 0;
-  for (let i = 0; i < parsedResponse.results.length; i++) {
-    pokemonArray[i] = parsedResponse.results[i].name;
-  }
-  console.log(parsedResponse);
-}
 
 async function printPhoto (pokemonID) {
     const response = await fetch(baseURL + '/pokemon/' + pokemonID);
@@ -159,7 +145,6 @@ async function printPhoto (pokemonID) {
 
     let photoURL = responseJSON.sprites.front_default;
     document.getElementById('poke').src = photoURL;
-    //console.log(photoURL);
 }
 
 
@@ -169,3 +154,4 @@ function modulo(number, base) {
       return result + base;
     return result;
 }
+
